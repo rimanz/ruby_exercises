89d1a1911144651c5c054a6e7d94de686f0b3bfb
Riman Das <34216663+rimanz@users.noreply.github.com>
2024-08-02 20:24:48 +0000

    Reorganize main.rb
    Project demonstrating codes are now enclosed in respective run_PROJECT_NAME method.

--- main.rb
+++ main.rb
@@ -1,14 +1,19 @@
 require './lib/caesar_cipher'
 
-puts 'Project: Caesar Cipher'
-puts '=' * 22
-puts
+def run_caesar_cipher 
+    puts 'Project: Caesar Cipher'
+    puts '=' * 22
+    puts
+    
+    puts 'Please enter your text: '
+    user_input = gets.chomp
+    
+    puts 'Please enter the shift (1~26): '
+    shift = gets.chomp.to_i
+    
+    CaesarCipher.caesar_cipher(user_input, shift)
+end
 
-puts 'Please enter your text: '
-user_input = gets.chomp
-
-puts 'Please enter the shift (1~26): '
-shift = gets.chomp.to_i
-
-output = CaesarCipher.caesar_cipher(user_input, shift)
-puts output
+if __FILE__ == $0
+    puts run_caesar_cipher
+end
